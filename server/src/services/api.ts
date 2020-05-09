daa402a6865fd2cfd53ef2bb92e63171f57520fb
Fatih Bulut <info@bulutfatih.com>
2020-05-09 23:29:54 +0300

    Added getReminders, createReminder, repo check APIs.

--- server/src/services/api.ts
+++ server/src/services/api.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express';
+import { Reminder } from '../models/reminder';
+import { MongooseDocument } from 'mongoose';
+import { VersionChecker } from './versionChecker';
 
 export class API {
   // @route   GET /
@@ -12,22 +15,38 @@ export class API {
   // @desc    Get all reminders
   // @access  Public
   getReminders(req: Request, res: Response) {
-    res.status(200).send('getReminders service.');
+    Reminder.find({}, (error: Error, reminder: MongooseDocument) => {
+      if (error) {
+        res.send(error);
+      }
+      res.json(reminder);
+    });
   }
 
   // @route   POST /createReminder
   // @desc    Create a reminder
   // @access  Public
   createReminder(req: Request, res: Response) {
-    console.log(req.body);
-    res.status(200).send('createReminder service.');
+    const newReminder = new Reminder(req.body);
+
+    newReminder.save((error: Error, reminder: MongooseDocument) => {
+      if (error) {
+        res.send(error);
+      }
+      res.json(reminder);
+    });
   }
 
   // @route   GET /:platform/:namespace/:repository
   // @desc    Check a repository
   // @access  Public
   async repo(req: Request, res: Response) {
-    console.log(req.params);
-    res.status(200).send('repo service.');
+    const { platform, namespace, repository } = req.params;
+
+    const versionChecker = new VersionChecker(platform);
+
+    const result = await versionChecker.checkPackages(namespace, repository);
+
+    res.json(result);
   }
 }
